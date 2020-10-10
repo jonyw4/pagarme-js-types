575d783ff4285ef964b7350a0859380dcc2ff82e
Ítalo Andrade <italodeandra@gmail.com>
2020-10-10 21:57:54 +0200

    fix(document): should not extend address (#35)

--- index.d.ts
+++ index.d.ts
@@ -487,7 +487,7 @@ declare module 'pagarme' {
   export type CustomerType = 'individual' | 'corporation' | 'other';
   export type DocumentType = 'cpf' | 'cnpj' | 'passaporte' | 'other';
 
-  export interface Document extends Address {
+  export interface Document {
     /** Tipo de documento. Para compradores brasileiros, deve ser fornecido ao menos um CPF (no caso de pessoa física, i.e. `individual`) ou CNPJ (no caso de pessoa jurídica, i.e. `corporation`). Para compradores internacionais, o documento pode ser um passaporte (type `passport`) ou um campo personalizado (type `other`). */
     type: DocumentType;
     /** Número do documento */
