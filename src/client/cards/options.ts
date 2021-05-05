6edc3d6e8815272cdc5f5ed578ca3db468e7026b
Arthur Abrantes <aabrantes@college.harvard.edu>
2021-05-05 17:31:34 -0300

    feat: create cards interfaces (#85)
    * add CardCreateOptions and Card interfaces
    
    * add CardFindOptions and CardAllOptions interfaces

---
+++ src/client/cards/options.ts
@@ -0,0 +1,26 @@
+export interface CardCreateOptions {
+    /** Número do cartão. */
+    card_number: string;
+    /** Data de expiração do cartão. */
+    card_expiration_date: string;
+    /** Nome no cartão do portador. */
+    card_holder_name: string;
+    /** Código de segurança do cartão. */
+    card_cvv?: string;
+    /** Informações do cliente do card a ser gerado. */
+    customer_id?: string;
+    /** Dados criptografados do cartão */
+    card_hash: string;
+}
+
+export interface CardFindOptions {
+    /** Id do cartão que deseja consultar os dados. */
+    id: string;
+}
+
+export type CardAllOptions = CardFindOptions & {
+    /** Pagination option to get a list of cards. Number of cards in a page */
+    count?: number;
+    /** Pagination option for a list of cards. The page index. */
+    page?: number;
+}
---
+++ src/client/cards/responses.ts
@@ -0,0 +1,30 @@
+import {Customer} from "../customers/responses";
+
+export interface Card {
+    /** Nome do tipo do objeto criado/modificado. */
+    object: "card";
+    /** Identificador do cartão. */
+    id: string;
+    /** Data de criação do objeto card. */
+    date_created: string;
+    /** Data de atualização do objeto card. */
+    date_updated: string;
+    /** Data de atualização do objeto card. */
+    brand: string;
+    /** Nome do portador do cartão. */
+    holder_name: string;
+    /** 6 primeiros dígitos do cartão. */
+    first_digits: string;
+    /** 4 últimos dígitos do cartão. */
+    last_digits: string;
+    /** País do cartão. */
+    country: string;
+    /** Hash que permite comparar dois cartões através de seus fingerprints para saber se são o mesmo. */
+    fingerprint: string;
+    /** Objeto com dados do comprador. */
+    customer: Customer | null;
+    /** Propriedade para verificar a validade do cartão, ou seja, caso true, é possível cobrar o cartão em condições normais, para false, não pode ser utilizado. */
+    valid: boolean;
+    /** Data de expiração do cartão. */
+    expiration_date: string;
+}
