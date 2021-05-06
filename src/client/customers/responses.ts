30331ed4d248523fd1ee5a4d023be8d0263470b9
Arthur Abrantes <aabrantes@college.harvard.edu>
2021-05-06 11:33:19 -0300

    fix: add missing fields to Customer and fix birthday in CustomerCreateOptiosn

--- src/client/customers/responses.ts
+++ src/client/customers/responses.ts
@@ -2,8 +2,20 @@ import { CustomerCreateOptions } from './options';
 import { Address } from '../../common/Address';
 
 export interface Customer extends CustomerCreateOptions {
+  /** Tipo do objeto */
+  object:  "customer",
   /** Identificador do cliente na loja */
   id: string;
+  /** Número do documento. */
+  document_number: number | null;
+  /** Tipo do documento. */
+  document_type: 'cpf' | 'cnpj' | 'passaport' | 'other' ;
+  /** Local de nascimento. */
+  born_at: string | null;
+  /** Gênero */
+  gender: string | null;
+  /** Quando o customer foi criado */
+  date_created: string;
   /** Lista dos telefones relacionados ao cliente */
   phones: string[];
   /** Lista de endereços relacionados ao cliente */
--- src/common/Document.ts
+++ src/common/Document.ts
@@ -1,4 +1,6 @@
 export interface Document {
+  object?: "document",
+  id?: "string",
   /**
    * Tipo de documento.
    *
