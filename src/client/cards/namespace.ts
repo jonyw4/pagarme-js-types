ef5d51f8f64d0818a56dff744e830758c8989823
Ezequiel Ramos <ezequielmr94@gmail.com>
2023-05-26 00:16:11 -0300

    adding typing for cards

--- src/client/cards/namespace.ts
+++ src/client/cards/namespace.ts
@@ -1,13 +1,44 @@
-import {CardCreateOptions, CardFindOptions, CardAllOptions} from "./options";
-import {Card} from "./responses";
+import { Options } from "../../common/Options";
+import { CardCreateOptions, CardFindOptions, CardAllOptions } from "./options";
+import { Card } from "./responses";
 
 declare module 'pagarme' {
   export namespace client {
     export namespace cards {
-      function all(pagination: CardAllOptions): Promise<Array<Card>>;
+      /**
+       * Makes a request to /cards
+       * @param opts An options params which is usually already bound by connect functions.
+       * @param body The payload for the request. API Reference for this payload (https://pagarme.readme.io/v1/reference#retornando-todos-os-cart%C3%B5es)
+       */
+      function all(opts: Options, body: CardAllOptions): Promise<Card[]>;
+      /**
+       * Makes a request to /cards
+       * @param body The payload for the request. API Reference for this payload (https://pagarme.readme.io/v1/reference#retornando-todos-os-cart%C3%B5es)
+       */
+      function all(body: CardAllOptions): Promise<Card[]>;
 
+      /**
+       * Creates a card from the given payload.
+       * @param opts An options params which is usually already bound by connect functions.
+       * @param body The payload for the request API Reference for this payload
+       */
+      function create(opts: Options, body: CardCreateOptions): Promise<Card>;
+      /**
+       * Creates a card from the given payload.
+       * @param body The payload for the request API Reference for this payload
+       */
       function create(body: CardCreateOptions): Promise<Card>;
 
+      /**
+       * Makes a request to /cards or to /cards/:id
+       * @param opts An options params which is usually already bound by connect functions.
+       * @param body The payload for the request. API Reference for this payload
+       */
+      function find(opts: Options, body: CardFindOptions): Promise<Card>;
+      /**
+       * Makes a request to /cards or to /cards/:id
+       * @param body The payload for the request. API Reference for this payload
+       */
       function find(body: CardFindOptions): Promise<Card>;
     }
   }
