c1e3bc89a22b33e6c85a047c36e7758ed133333c
Ezequiel Ramos <ezequielmr94@gmail.com>
2023-05-27 16:03:51 -0300

    adding plan types

--- src/client/plans/namespace.ts
+++ src/client/plans/namespace.ts
@@ -1,15 +1,38 @@
+import { Options } from "../../common/Options";
+import { PlansAllOptions, PlansFindOptions, PlansUpdateOptions } from './options';
+
 declare module 'pagarme' {
   export namespace client {
     export namespace plans {
-      function all(opts: any, pagination: any): any;
+      /**
+       * Makes a request to /plans
+       * @param opts An options params which is usually already bound by connect functions.
+       * @param body The payload for the request. API Reference for this payload (https://pagarme.readme.io/v1/reference#retornando-planos)
+       */
+      function all(opts: Options, body: PlansAllOptions): Promise<any>;
 
-      function create(opts: any, body: any): any;
+      /**
+       * Creates a plan from the given payload.
+       * @param opts An options params which is usually already bound by connect functions.
+       * @param body The payload for the request API Reference for this payload (https://pagarme.readme.io/v1/reference#criando-planos)
+       */
+      function create(opts: Options, body: object): Promise<any>;
 
-      function find(opts: any, body: any): any;
+      /**
+       * Makes a request to /plans or to /plans/:id
+       * @param opts An options params which is usually already bound by connect functions.
+       * @param body The payload for the request. API Reference for this payload (https://pagarme.readme.io/v1/reference#retornando-um-plano)
+       */
+      function find(opts: Options, body: PlansFindOptions): Promise<any>;
 
       function findAll(a0: any, a1: any, ...args: any[]): any;
 
-      function update(opts: any, body: any): any;
+      /**
+       * Updates a plans from the given payload.
+       * @param opts An options params which is usually already bound by connect functions.
+       * @param body The payload for the request API Reference for this payload (https://pagarme.readme.io/v1/reference#atualizando-planos)
+       */
+      function update(opts: Options, body: PlansUpdateOptions): Promise<any>;
     }
   }
 }
